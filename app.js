501372ded87ca05f804c158337c139bdd7c74102
Edgar <bvcott@outlook.com>
2020-03-13 01:07:53 +0000

    Further development of Fashion Pet's Website, now implementing ejs for better development workflow purposes.

---
+++ app.js
@@ -0,0 +1,28 @@
+const ejs = require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+//////////////////////////// Home Page ////////////////////////////
+
+app.route("/")
+
+.get(function(req, res) {
+  res.render("home")
+});
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server running on port 3000...");
+});
--- index.js
+++
@@ -1,9 +0,0 @@
-@media only screen and (max-width: 600px) {
-    
-$("#containerTitulo").removeClass("col-md-6");
-
-
-
-
-
-  }
